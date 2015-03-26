f7c107a003ae644782d85b9f5115965f50f8f5dc
Philip Basford <pjb@ecs.soton.ac.uk>
2015-03-26 21:13:24 +0000

    Will now take an argument to specify which sensor

--- graph.py
+++ graph.py
@@ -1,6 +1,16 @@
+from mod_python import util
+
+BASE_URL = "http://data.mountainsensing.org/feshie/data/data.py"
+BASE_TITLE = "Feshie Mountain Sensing: "
 
 def index(req):
-    url = "http://data.mountainsensing.org/feshie/data/data.py?sensor=temp"
+    parameters = util.FieldStorage(req, keep_blank_values=1)
+    if not "sensor" in parameters.keys():
+        return "Must specify sensor type"
+    sensor = parameters.getfirst("sensor")
+    if sensor in ["temperature", "temp"]:
+        url = BASE_URL + "?sensor=temp"
+        title = BASE_TITLE + "Temperature"
     output = ""
     output += """
     <!DOCTYPE html>
@@ -9,16 +19,16 @@ def index(req):
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7; IE=EmulateIE9"> 
         <!--[if IE]><script src="excanvas.js"></script><![endif]-->
-    <title> Glacsweb TinyTag Data """
-
+    <title> """
+    output += title
     output += """
     </title>
     <script type="text/javascript"
       src="dygraph-combined.js"></script>
     </head>
     <body>
-    <h1> Glacsweb Davis Tinytag Data"""
-
+    <h1>"""
+    output += title
     output +="""</h1>
     <p>To zoom on the graph highlight the region you would like to zoom in on, this works with both the X and Y axis.  To return the graph to showing all data double click on it</p>
     <div id="tempdiv"></div>
