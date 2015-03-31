6a13fd761f2735cb7d5d20c6823860c8149572c7
Philip Basford <pjb@ecs.soton.ac.uk>
2015-03-31 17:03:10 +0100

    Now has live section

--- index.py
+++ index.py
@@ -39,5 +39,36 @@ def index(req):
 		output += "<td><a href = \"graph.py?test&sensor=chain&node=%s\">Chain</a></td>" % node
 		output +="</tr>"
 	output +="</table>"
+
+
+
+	output += "<h2>LIVE Data</h2>"
+	LIVE_DUMPER = DataDump(LIVE_CONFIG)
+	LIVE_LATEST = LIVE_DUMPER.get_latest_readings()
+	output += """<table><tr><th>Node</th><th>Latest</th></tr>"""
+	for node in LIVE_LATEST:
+		output +="<tr><td>%s</td><td>%s</td></tr>"% (node[0], node[1])
+	output +="</table>"
+	output += """
+	<h3>Combined Graphs</h3>
+	<a href="graph.py?test&sensor=temp">Temperature</a><br/>
+	<a href="graph.py?test&sensor=batt">Battery</a><br/>
+	<a href="graph.py?test&sensor=adc&adc_id=1">ADC 1</a><br/>
+	<a href="graph.py?test&sensor=adc&adc_id=2">ADC 2</a><br/>
+	<a href="graph.py?test&sensor=moisture">Moisture</a><br/>
+	<a href="graph.py?test&sensor=rain">Rainfall</a><br/>
+	"""
+	
+	LIVE_NODES = LIVE_DUMPER.get_nodes()
+	output += "<h3>Node specific graphs</h3>"
+	output += """<table>"""
+	for node in LIVE_NODES:
+		output += "<tr><td>%s</td>" % node
+		output += "<td><a href = \"graph.py?test&sensor=accel&node=%s\">Accelerometer</a></td>" % node
+		output += "<td><a href = \"graph.py?test&sensor=ow&node=%s\">Spider</a></td>" % node
+		output += "<td><a href = \"graph.py?test&sensor=wp&node=%s\">Water Pressure</a></td>" % node
+		output += "<td><a href = \"graph.py?test&sensor=chain&node=%s\">Chain</a></td>" % node
+		output +="</tr>"
+	output +="</table>"
 	output +="</body></html>"
 	return output
